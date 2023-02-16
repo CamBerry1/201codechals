58ca146a685196dba221f27b7f1b0b86fa1f2171
CamBerry1 <cberry2008@gmail.com>
2023-02-15 19:02:48 -0800

    Class 02 Ops Challenge

---
+++ helloworld.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Cameron Berry
+# Date of latest revision: 2/15/23
+# Purpose: Print a string to the terminal
+
+# Main
+
+echo "I am working perfectly"
+
+# End
