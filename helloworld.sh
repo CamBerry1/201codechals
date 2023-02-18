034da6c0cec54fd80a3c897bd4d51d584bdc3ae7
CamBerry1 <cberry2008@gmail.com>
2023-02-18 09:47:47 -0800

    Cleaned up intro

--- helloworld.sh
+++ helloworld.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Script: Ops 201 Class 02 Ops Challenge Solution
-# Author: Cameron Berry
-# Date of latest revision: 2/15/23
-# Purpose: Print a string to the terminal
+# Script:                   Ops 201 Class 02 Ops Challenge Solution
+# Author:                   Cameron Berry
+# Date of latest revision:  2/15/23
+# Purpose:                  Print a string to the terminal
 
 # Main
 
