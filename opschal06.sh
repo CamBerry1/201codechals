df1579a47b1a42934e83ffc4312d56e839dc076e
CamBerry1 <cberry2008@gmail.com>
2023-03-01 20:27:12 -0800

    Ops challenge 06

---
+++ opschal06.sh
@@ -0,0 +1,23 @@
+# !/bin/bash
+
+# Script:                201 Ops Challenge 06 Solution
+# Author:                Cameron Berry
+# Date of last revision: 3/1/23
+# Purpose:               Create a script that detects if a file or directory exists,
+#                        then creates it if it does not exist.
+#                        Must contain at least 1:
+#                           Array
+#                           Loop
+#                           Conditional
+
+dir_array=("mydir" "newdir" "dirdir")
+
+for dir in ${dir_array[*]}
+do
+    if [ -d $dir ]; then
+        echo "Directories already exist"
+        break
+    else
+        mkdir "$dir"
+    fi
+done
